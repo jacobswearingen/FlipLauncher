dbf925b5026c27d0af7800662f4206da4d8e1110
Jacob Swearingen <flobbus@gmail.com>
2026-01-29 00:24:03 -0600

    optimize imports

--- app/src/main/java/com/jacobswearingen/fliplauncher/AppListFragment.kt
+++ app/src/main/java/com/jacobswearingen/fliplauncher/AppListFragment.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import android.widget.ImageView
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
